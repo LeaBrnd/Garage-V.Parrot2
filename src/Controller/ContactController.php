b5cb02de45412c25f72bdcacf12b98a012a6a354
Lea <leajustine17@gmail.com>
2023-07-16 13:22:07 +0200

    Formulaire de contact fonctionnel

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request, MailerInterface $mailer): Response
+
+    {
+        $form = $this-> createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+        $address = $form->get('email')->getData();
+        $subject = $form->get('subject')->getData();
+        $content = $form->get('content')->getData();
+
+        $email = (new Email())
+        ->from($address)
+        ->to('admin@sitegarage.com')
+        ->subject($subject)
+        ->text($content);
+
+        $mailer->send($email);
+
+        return $this->redirectToroute('app_sucess');
+
+        }
+        return $this->renderForm('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'form' => $form
+        ]);
+    }
+    
+        #[Route('/contact/sucess', name: 'app_sucess')]
+    public function sucess(): Response
+    {
+        return $this->render('sucess/index.html.twig', [
+            'controller_name' => 'SucessController',
+        ]);
+    }
+}
---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Form\User1Type;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/profil')]
+class ProfilController extends AbstractController
+{
+    #[Route('', name: 'app_profil_show', methods: ['GET'])]
+    public function show(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('profil/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_profil_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager,UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(User1Type::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $hashPassword = $passwordHasher->hashPassword($user,$plainPassword);
+            $user->setPassword($hashPassword);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profil_show', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profil/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+}
